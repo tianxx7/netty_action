61635ec40b8ab8027d4b989b274f3df89791ed45
tian_xx <1729549607@qq.com>
2019-09-19 20:44:24 +0800

    nio 中文编码示例

--- src/main/java/txx/java/nio_demo/NioTest2.java
+++ src/main/java/txx/java/nio_demo/NioTest2.java
@@ -1,10 +1,11 @@
 package txx.java.nio_demo;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 
 public class NioTest2 {
     public static void main(String[] args) throws IOException {
@@ -14,11 +15,11 @@ public class NioTest2 {
         ByteBuffer buffer = ByteBuffer.allocate(512);//512字节
         fileChannel.read(buffer);//只读512字节的内容,后面的文本不读取
         buffer.flip();
+        byte[] bytes = new byte[512];
         while (buffer.remaining() > 0) {
-            byte b = buffer.get();
-            System.out.println("Character: " + (char)b);//不支持中文,中文需要编码
+            buffer.get(bytes,0,buffer.limit());
+            System.out.println("Character: " + new String(bytes, Charset.forName("UTF-8")));//不支持中文,中文需要编码
         }
-        buffer.clear();
         buffer = null;
         fileInputStream.close();
     }
